9f0eae1f3bc7e6a40150cc1e3c47593e637f24e7
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-04-06 21:27:55 +0200

    Add script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var add = document.getElementById('addElem');
+	list = document.getElementById('list');
+
+
+add.addEventListener('click', function(){
+	var element = document.createElement('li');
+		level = document.getElementsByTagName('li');
+		currentLevel=level.length		
+	element.innerHTML = 'item ' + currentLevel;
+	list.appendChild(element);
+});
