87083f239c3b888e7b3f14d6f69f32e6e714acb7
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-04-10 21:25:34 +0200

    Edit scrip - function corection

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-var add = document.getElementById('addElem');
+var add = document.getElementById('addElem'),
 	list = document.getElementById('list');
 
 
-add.addEventListener('click', function(){
-	var element = document.createElement('li');
-		level = document.getElementsByTagName('li');
-		currentLevel=level.length		
+add.addEventListener('click', function () {
+	var element = document.createElement('li'),
+		level = document.getElementsByTagName('li'),
+		currentLevel = level.length;
+
 	element.innerHTML = 'item ' + currentLevel;
 	list.appendChild(element);
 });
